2239293980c08f12985d7bd6ada146580fa3aeb6
Mich-Dich <mally.dominik@gmx.de>
2024-02-15 16:34:14 +0100

    Update visual style

--- ToDo/App.xaml.cs
+++ ToDo/App.xaml.cs
@@ -1,7 +1,18 @@
-﻿using System.Windows;
+﻿using System.Security.AccessControl;
+using System.Windows;
 
 namespace ToDo {
 
-    public partial class App : Application { }
+	public class Test {
+
+		public void Test_func() { }
+	}
+
+
+    public partial class App : Application {
+		
+		// Test custom = new Test();
+        // custom.Test_func();
+	}
 
 }
--- ToDo/MainWindow.xaml.cs
+++ ToDo/MainWindow.xaml.cs
@@ -10,5 +10,29 @@ namespace ToDo
         {
             InitializeComponent();
         }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+
+            DragMove();
+        }
+
+        private void BU_minimize_Click(object sender, RoutedEventArgs e) {
+
+            WindowState = WindowState.Minimized;
+        }
+
+        private void BU_close_Click(object sender, RoutedEventArgs e) {
+
+            Close();
+        }
+
+        private void BU_maximize_Click(object sender, RoutedEventArgs e) {
+
+            if(WindowState == WindowState.Maximized) 
+                WindowState = WindowState.Normal; 
+            else 
+                WindowState = WindowState.Maximized;
+        }
+
     }
 }
